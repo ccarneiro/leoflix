331d5915ff99088bdf6564ac9e86eecf734da5c5
Leonardo Carneiro <ccarneiro@gmail.com>
2020-07-31 00:13:56 -0300

    Correções e desafio

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import styled, { keyframes } from 'styled-components';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import Button from '../../../components/Button';
+import Ju from '../../../assets/img/juliana.png';
+import Marcos from '../../../assets/img/marcos.png';
+
+const URL_API = window.location.hostname.includes('localhost')
+  ? 'http://localhost:8080/categorias'
+  : 'https://leoflix-app.herokuapp.com/categorias';
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const LoadImage = styled.img`
+  clip-path: circle(60px at center);
+  animation: ${rotate} 2s linear infinite;
+`;
 
 function CadastroCategoria() {
   const categoriaInicial = {
@@ -11,7 +33,8 @@ function CadastroCategoria() {
     cor: '#000000',
   };
   const [categoria, setCategoria] = useState(categoriaInicial);
-  const [categorias, setCategorias] = useState([{ nome: 'Teste' }]);
+  const [categorias, setCategorias] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const setValue = (nome, valor) => {
     // console.log("event.target", event.target);
@@ -28,16 +51,44 @@ function CadastroCategoria() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setCategorias([...categorias, categoria]);
-    setCategoria(categoriaInicial);
+    fetch(URL_API, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(categoria) })
+      .then(() => {
+        setLoading(true);
+        return fetch(URL_API);
+      })
+      .then((response) => response.json())
+      .then((cats) => {
+        setLoading(false);
+        console.log('cats', cats);
+        setCategorias(cats);
+        setCategoria(categoriaInicial);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.error(err);
+      });
+
+    // setCategorias([...categorias, categoria]);
+    // setCategoria(categoriaInicial);
   };
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    // const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://leoflix-app.herokuapp.com/categorias';
+    setLoading(true);
     fetch(URL)
       .then(async (response) => {
         const categoriasList = await response.json();
-        setCategorias([...categoriasList]);
+        setTimeout(() => {
+          setLoading(false);
+          setCategorias([...categoriasList]);
+        }, 2000);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.error(err);
       });
   }, []);
 
@@ -68,6 +119,13 @@ function CadastroCategoria() {
         />
         <Button>Cadastrar</Button>
       </form>
+
+      {loading && (
+        <div>
+          <LoadImage src={Ju} alt="Ju" />
+          <LoadImage src={Marcos} alt="Ju" />
+        </div>
+      )}
 
       <ul>
         {categorias.map((cat, i) => (
